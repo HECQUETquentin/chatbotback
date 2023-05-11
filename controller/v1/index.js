eee1b6df4ed43cab5bff69145d4be844611f43e3
Quentin Hecquet <quentin.hecquet@outlook.fr>
2023-05-11 15:10:14 +0200

    add controller route

--- controller/v1/index.js
+++ controller/v1/index.js
@@ -18,6 +18,34 @@ const dialogController = {
             }
         ]
         res.status(200).json({ message: dialogs })
+    },
+    dialogp: (req, res) => { 
+        console.log(req.body.question)
+        let matchFound=false;
+        const dialogs=[
+            {
+                question : "salut",
+                answer : "coucou"
+            },
+            {
+                question : "ca va?",
+                answer : "oui et toi?"
+            },
+            {
+                question : "quel age as tu?",
+                answer : "22 ans"
+            }
+        ]
+        dialogs.forEach(dialog =>{
+            if(dialog.question === req.body.question){
+                matchFound = true;
+                res.status(200).json({Response : dialog.answer})
+                return
+            }
+        })
+        if(!matchFound){
+            res.status(200).json({message: "pas de réponse a vous apporter"})
+        }
     }
 }
 
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -6,33 +6,6 @@ router.get('/', dialogController.home)
 
 router.get('/dialogs', dialogController.dialogs)
 
-router.post('/dialogs', (req, res) => { 
-    console.log(req.body.question)
-    let matchFound=false;
-    const dialogs=[
-        {
-            question : "salut",
-            answer : "coucou"
-        },
-        {
-            question : "ca va?",
-            answer : "oui et toi?"
-        },
-        {
-            question : "quel age as tu?",
-            answer : "22 ans"
-        }
-    ]
-    dialogs.forEach(dialog =>{
-        if(dialog.question === req.body.question){
-            matchFound = true;
-            res.status(200).json({Response : dialog.answer})
-            return
-        }
-    })
-    if(!matchFound){
-        res.status(200).json({message: "pas de réponse a vous apporter"})
-    }
-})
+router.post('/dialogs', dialogController.dialogp)
 
 module.exports = router
